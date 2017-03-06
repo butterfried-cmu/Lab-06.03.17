3c1163acc8a77ab1f83659daa7ab54e4ae2745fe
LAB211-11\CAMT <Kittipan_panyapusit@cmu.ac.th>
2017-03-06 18:16:42 +0700

    no message

---
+++ labcherearaimairukutummaidai/Currency.java
@@ -0,0 +1,10 @@
+package utility;
+
+/**
+ * Created by CAMT on 6/3/2560.
+ */
+public class Currency {
+    public double exchange (String sourceCurrency, double amount, String targetCurrency){
+        return 0.0;
+    }
+}
