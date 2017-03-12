1217b624809dceb0da4a3586f321c060539d6c8f
AzureAD\TEEPOPUEANGSAWAT <teepop_u@cmu.ac.th>
2017-03-13 00:22:43 +0700

    implement method when source is USD

--- labcherearaimairukutummaidai/src/main/java/Currency.java
+++ labcherearaimairukutummaidai/src/main/java/Currency.java
@@ -7,4 +7,16 @@ public class Currency {
     public double exchange (String sourceCurrency, double amount, String targetCurrency){
         return 0.0;
     }
+
+
+//C
+private double sourceIsUSD (String sourceCurrency, double amount, String targetCurrency){
+	if(targetCurrency=="THB"){
+		amount = amount * 33.0;
+		return amount;
+	}else{
+		amount = amount * 1.01;
+		return amount;
+	}
+}
 }
