77af5286eb39eabd567486f3778ec51c6f546992
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-13 01:45:04 +0700

    update 2

--- labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
+++ labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
@@ -13,16 +13,16 @@ public class GradeCalculatorTest {
         GradeCalculator gc = new GradeCalculator();
 	    assertThat(gc.calculate(25,28,32),is('B'));
         assertThat(gc.calculate(0,7,32,5),is('F'));
-        assertThat(gc.calculate(20,21,32,20),is('A'));
+        assertThat(gc.calculate(20,21,32,25),is('A'));
         assertThat(gc.calculate(100,28),is('x'));
 	    assertThat(gc.calculate(25,28,32),is('B'));
         assertThat(gc.calculate(90),is('B'));
 	    assertThat(gc.calculate(65,1),is('C'));
         assertThat(gc.calculate(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),is('C'));
 	    assertThat(gc.calculate(25,28,32),is('B'));
-        assertThat(gc.calculate(40, 26),is('F'));
+        assertThat(gc.calculate(40, 26),is('C'));
         assertThat(gc.calculate(20,12,6),is('F'));
         assertThat(gc.calculate(80,35,5),is('x'));
-        assertThat(gc.calculate(80),is('B'));
+        assertThat(gc.calculate(81),is('B'));
     }
 }
