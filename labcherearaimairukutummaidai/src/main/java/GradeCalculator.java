efb4f4f747e7909adebe8c2b8f6311925005b6c9
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-13 01:37:21 +0700

    update

--- labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
+++ labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
@@ -7,16 +7,15 @@ public class GradeCalculator {
 		double sum = 0;
 		for (int i = 0 ; i < scores.length ; i++){
 			sum = sum + scores[i];
-			
+		}
 		// B implement code to return right output from sum
 		if(sum <= 100 && sum >= 0){
 			if (sum > 95) return 'A';
 			if (sum > 80) return 'B';
 			if (sum > 60) return 'C';
 				return 'F';
-			}else {
-				return 'x';
-			}
+		}else {
+			return 'x';
 		}
-	}
+    }
 }
