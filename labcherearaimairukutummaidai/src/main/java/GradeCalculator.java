70ea8633654ad0795e8f8d151e2156ae42244bbf
DESKTOP-3V808EP\JameZ <Kittipan_panyapusit@cmu.ac.th>
2017-03-13 01:41:46 +0700

    no message

--- labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
+++ labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
@@ -5,9 +5,9 @@ public class GradeCalculator {
     public char calculate(double... scores){
 		// A implement code to find summation of all scores to sum
 		double sum = 0;
-		for (int i = 0 ; i < scores.length ; i++){
+		for (int i = 0 ; i < scores.length ; i++) {
 			sum = sum + scores[i];
-
+		}
 		// B implement code to return right output from sum
 		if(sum <= 100 && sum >= 0){
 			if (sum > 95) return 'A';
