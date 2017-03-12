e65289a0b2b2536b685564f842dbbd659f68d543
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-13 00:27:42 +0700

    add implement method when target is USD

--- labcherearaimairukutummaidai/src/main/java/Currency.java
+++ labcherearaimairukutummaidai/src/main/java/Currency.java
@@ -7,4 +7,14 @@ public class Currency {
     public double exchange (String sourceCurrency, double amount, String targetCurrency){
         return 0.0;
     }
+
+private double targetIsUSD (String sourceCurrency, double amount, String targetCurrency){
+	if(sourceCurrency=="THB"){
+		amount = amount/33.0;
+		return amount;
+	}else{
+		amount = amount / 1.01;
+		return amount;
+	}
+}
 }
