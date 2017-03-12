9dc8076037f3667b8607931e4211829be4a27a10
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-12 23:43:22 +0700

    add test case

--- labcherearaimairukutummaidai/src/test/java/CurrencyTest.java
+++ labcherearaimairukutummaidai/src/test/java/CurrencyTest.java
@@ -11,6 +11,8 @@ import static org.hamcrest.number.IsCloseTo.closeTo;
 public class CurrencyTest {
     public void testExchange(){
         Currency currency = new Currency();
-       assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
+       	assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
+	assertThat(currency.exchange("THB",99.0,"USD"),closeTo(3.0,0.001));
+	assertThat(currency.exchange("THB",66.0,"EUR"),closeTo(2.02,0.001));
     }
 }
