2bfc2bd7311085b70c319e1dcd346850b961d6f2
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-13 01:12:24 +0700

    add grade test case

--- labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
+++ labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
@@ -12,5 +12,8 @@ public class GradeCalculatorTest {
     public void testCalculator(){
         GradeCalculator gc = new GradeCalculator();
 	    assertThat(gc.calculate(25,28,32),is('B'));
+        assertThat(gc.calculate(0,7,32,5),is('F'));
+        assertThat(gc.calculate(20,21,32,20),is('A'));
+        assertThat(gc.calculate(100,28),is('x'));
     }
 }
