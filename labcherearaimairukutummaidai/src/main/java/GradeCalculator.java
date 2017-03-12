24bcd03afa24858beec40b4858bd3fd7fa768154
LEVEL51-RPM3M22\MyLevel51 <teepop_u@cmu.ac.th>
2017-03-13 01:26:07 +0700

    add implement code : grade calculator

--- labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
+++ labcherearaimairukutummaidai/src/main/java/GradeCalculator.java
@@ -5,9 +5,17 @@ public class GradeCalculator {
     public char calculate(double... scores){
 
 		double sum = 0;
+
 		// A implement code to find summation of all scores to sum
 
-
 		// B implement code to return right output from sum
+		if(sum <= 100 && sum >= 0){
+			if (sum > 95) return 'A';
+			if (sum > 80) return 'B';
+			if (sum > 60) return 'C';
+			return 'F';
+		}else {
+			return 'x';
+		}
 	}
 }
