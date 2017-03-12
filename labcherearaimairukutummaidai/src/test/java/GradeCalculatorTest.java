af0071fa4e561ac8cd65e325b12787296add9922
DESKTOP-3V808EP\JameZ <Kittipan_panyapusit@cmu.ac.th>
2017-03-13 01:06:26 +0700

    test grade

--- labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
+++ labcherearaimairukutummaidai/src/test/java/GradeCalculatorTest.java
@@ -11,6 +11,10 @@ public class CurrencyTest {
     @Test
     public void testCalculator(){
         GradeCalculator gc = new GradeCalculator();
-	assertThat(gc.calculate(25,28,32),is('B'));
+	    assertThat(gc.calculate(25,28,32),is('B'));
+        assertThat(gc.calculate(40, 26),is('D'));
+        assertThat(gc.calculate(20,12,6),is('F'));
+        assertThat(gc.calculate(80,35,5),is('X'));
+        assertThat(gc.calculate(80),is('B'));
     }
 }
